f6bab2d9af8c73cc37ba52ee89422fd22989bd08
HansikaPerera <madhuhanz@gmail.com>
2021-02-15 10:50:59 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialModule } from './material.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountryTableComponent } from './country-table/country-table.component';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    WelcomeComponent,
+    DashboardComponent,
+    CountryTableComponent,
+    DialogBoxComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MaterialModule,
+        FlexLayoutModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatInputModule,
+        MatDialogModule,
+        MatTableModule,
+        MatPaginatorModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
