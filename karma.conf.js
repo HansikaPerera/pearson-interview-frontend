eb9a0acc30053f1e2f244e99460932ad7fa16f22
HansikaPerera <madhuhanz@gmail.com>
2021-02-17 00:38:19 +0300

    Fixed application title

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/pearson-interview-froontend'),
+      dir: require('path').join(__dirname, './coverage/pearson-interview-frontend'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
